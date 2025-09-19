eba9f63bb9915a5a61646c2a4a859f740fabb48a
Alvaro Houx <alvarohoux@gmail.com>
2025-09-19 12:35:04 -0300

    refactor(test): move tryLogin to a shared test module
    The `tryLogin` function was moved from `materias.test.ts` to `login.test.ts` to be reused by other tests.

--- __tests__/materias.test.ts
+++ __tests__/materias.test.ts
@@ -1,18 +1,6 @@
 import { MateriasPeriodoAtual } from "../dist/materias";
 import { getCronograma } from "../dist/cronograma";
-import { Login } from "../dist/login";
-import { exitCode } from "process";
-
-
-async function tryLogin() {
-  const username = process.env.APP_USERNAME;
-  const password = process.env.APP_PASSWORD;
-
-  if (!username || !password)
-    throw new Error("As variáveis de ambiente APP_USERNAME e APP_PASSWORD devem ser definidas para este teste.");
-
-  return await Login(username, password);
-}
+import { tryLogin } from "./login.test";
 
 describe("materias", () => {
   it("deve conseguir as materias do usuário", async () => {
@@ -35,7 +23,7 @@ describe("materias", () => {
 describe("cronograma", () => {
   it("deve conseguir o cronograma do usuário", async () => {
     const res = await tryLogin();
-    
+
     if (res.sucesso && res.token) {
       const cronograma = await getCronograma(res.token);
       expect(cronograma).toBeDefined();
